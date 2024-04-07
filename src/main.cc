926b0c3d33eead8a3fe3db081af2c8bada6a5cfa
Segfault (Core dumped) <tyiwei0@gmail.com>
2024-04-07 23:30:49 +0800

    Created index.yml parser.

--- src/main.cc
+++ src/main.cc
@@ -21,6 +21,19 @@ std::string unext(std::string str) {
   return str.substr(0, value);
 }
 
+inline std::string trim(std::string&& in) {
+  in.erase(std::find_if(in.rbegin(), in.rend(),
+                        [](unsigned char ch) { return !std::isspace(ch); })
+               .base(),
+           in.end());
+
+  in.erase(in.begin(), std::find_if(in.begin(), in.end(), [](unsigned char ch) {
+             return !std::isspace(ch);
+           }));
+
+  return in;
+}
+
 inline void rtrim(std::string& in) {
   in.erase(std::find_if(in.rbegin(), in.rend(),
                         [](unsigned char ch) { return !std::isspace(ch); })
@@ -43,31 +56,36 @@ int main(int argc, const char** argv, const char** envp) {
   }
 
   sqlite3_exec(database,
-               "CREATE TABLE IF NOT EXISTS   charts ("
-               "  index INTEGER PRIMARY KEY NOT NULL,"
-               "  identifier TEXT NOT NULL"
-               "  title TEXT NOT NULL,"
-               "  composer TEXT NOT NULL,"
-               "  charter TEXT NOT NULL,"
-               "  alias TEXT,"
-               "  illustrator TEXT,"
-               "  chartConstant REAL NOT NULL,"
-               "  difficulty TEXT,"
-               "  displayedConstant TEXT,"
-               "  baseBPM INTEGER NOT NULL,"
-               "  bpmText TEXT,"
-               "  side INTEGER NOT NULL,"
-               "  searchTags TEXT"
-               "  pack TEXT"
+               "CREATE TABLE IF NOT EXISTS main.charts (\n"
+               "id INTEGER PRIMARY KEY,\n"
+               "  identifier TEXT NOT NULL,\n"
+               "  title TEXT NOT NULL,\n"
+               "  composer TEXT NOT NULL,\n"
+               "  charter TEXT NOT NULL,\n"
+               "  alias TEXT,\n"
+               "  illustrator TEXT,\n"
+               "  chartConstant REAL NOT NULL,\n"
+               "  difficulty TEXT,\n"
+               "  displayedConstant TEXT,\n"
+               "  baseBPM INTEGER NOT NULL,\n"
+               "  bpmText TEXT,\n"
+               "  side INTEGER NOT NULL,\n"
+               "  searchTags TEXT pack TEXT\n"
                ");",
                nullptr, nullptr, &errmsg);
 
-  if (int err = system((u8"7z x " + std::string(argv[1]) + " -otmp/" +
-                        unext(std::string(argv[1])) + " -y -bb0 > tmp/del.txt")
-                           .c_str())) {
-    std::cerr << "Unable to extract file.";
-    return err;
+  if (errmsg) {
+    std::cout << errmsg;
   }
+
+  // // Cancer
+  // if (int err = system((u8"7z x " + std::string(argv[1]) + " -otmp/" +
+  //                       unext(std::string(argv[1])) + " -y -bb0 >
+  //                       tmp/del.txt")
+  //                          .c_str())) {
+  //   std::cerr << "Unable to extract file.";
+  //   return err;
+  // }
 
   std::filesystem::path working =
       std::filesystem::current_path() / "tmp" /
@@ -96,11 +114,56 @@ int main(int argc, const char** argv, const char** envp) {
     index.close();
   }
 
-    for (lines chart : pack) {
+  std::vector<apkg::chart> charts{};
+  std::vector<std::string> directories{};
+  std::vector<std::string> settingsFile{};
+
+  {
+    std::string packName;
+    size_t packIdx = -1;
+    for (size_t i = 0; i < pack.size(); ++i) {
+      for (std::string& config : pack[i]) {
+        if (config.substr(0, 4) == "type") {
+          if (trim(config.substr(6)) == "pack") {
+            packIdx = charts.size() - 1;
+          }
+        } else if (config.substr(0, 9) == "directory") {
+          directories.push_back(config.substr(11));
+        } else if (config.substr(0, 10) == "identifier") {
+          charts.push_back(apkg::chart(trim(config.substr(12))));
+        } else if (config.substr(0, 12) == "settingsFile") {
+          settingsFile.push_back(config.substr(14));
+        } else if (config.substr(0, 7) == "version") {
+          continue;
+        }
+      }
+    }
+
+    if (packIdx != -1ull) {
+      charts.erase(charts.begin() + packIdx);
+      settingsFile.erase(settingsFile.begin() + packIdx);
+
+      for (apkg::chart& chart : charts) {
+        chart.pack = directories[packIdx];
+      }
+
+      directories.erase(directories.begin() + packIdx);
+    }
+  }
+
+  for (apkg::chart& chart : charts) {
+    std::cout << chart.identifier << "\n" << chart.pack;
+  }
+
+#if 0
+
+  for (lines chart : pack) {
     for (std::string config : chart) {
       std::cout << config << "\n";
     }
   }
+
+#endif
 
   return 0;
 }
